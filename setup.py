a79bc2dad662b3d1425ebcd98a2dc752a80b9843
ashitov <ashitov@mlg.ru>
2018-08-28 17:54:27 +0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='timer',
+    version='1.0',
+    packages=find_packages(),
+    long_description='Timer for timing, made by lobziq',
+)
