b81ccb6e6386ff0b22245095274caeb09409762b
ashitov <ashitov@mlg.ru>
2018-08-28 18:03:54 +0300

    setup py info

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='timer',
+    name='ltimer',
     version='1.0',
     packages=find_packages(),
     long_description='Timer for timing, made by lobziq',
